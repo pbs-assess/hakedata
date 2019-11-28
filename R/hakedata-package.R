b0d8a1602050f2800aafa54b64ac4c011dbb5235
Chris Grandin <chris.grandin@gmail.com>
2019-11-28 13:28:48 -0800

    Add more explanation to the package details in help docs

--- R/hakedata-package.R
+++ R/hakedata-package.R
@@ -1,4 +1,9 @@
-#' hakedata package - Extract Pacific hake data and plot it
+#' hakedata package
+#'
+#' @description Extract Pacific hake data and plot it. Follow the instructions here:
+#'  \url{https://github.com/pbs-assess/hake} to extract data, and see the rmarkdown file:
+#'  \url{https://github.com/pbs-assess/hake/blob/master/hake-data.Rmd} for an example.
+#'
 #' @docType package
 #' @name hakedata
 NULL
